c757e6050b9d76133a1cd35fb0f6e3c6c494aa24
Chris Stavitsky <corridorr@gmail.com>
2022-11-11 16:56:05 -0800

    additional sentry context (partial)

--- dogwalk.rb
+++ dogwalk.rb
@@ -8,7 +8,7 @@ require './csv_parser.rb'
 puts "Starting scraping."
 # companies = YCClient.company_data #leaving as example
 companies = SalesforceCSVParser.new("./dog_walker_v2_11_9_22.csv").company_data
-companies = companies[0...10] #truncate while i test if this works. remove line when running for real
+companies = companies[0...2] #truncate while i test if this works. remove line when running for real
 problems_scraping = nil
 formatted_list = []
 companies.each do |company|
@@ -27,20 +27,23 @@ companies.each do |company|
   
   sniffed_data = observability_products_detected.data
   org_details = {
-    company: company.name,
-    website: company.website,
-    uses_sentry: sniffed_data["sentry"][:detected],
-    uses_rollbar: sniffed_data['rollbar'][:detected],
-    uses_logrocket: sniffed_data['logrocket'][:detected],
-    uses_newrelic: sniffed_data['newrelic'][:detected],
-    uses_datadog: sniffed_data['datadog'][:detected],
-    uses_bugsnag: sniffed_data['bugsnag'][:detected],
     # segment: company.segment,
     # total_touch_arr: company.total_touch_arr,
     id: company.id,
     owner: company.owner,
     detected_on: detected_on,
-    problems_scraping: problems_scraping
+    problems_scraping: problems_scraping,
+    company: company.name,
+    website: company.website,
+    uses_rollbar: sniffed_data['rollbar'][:detected],
+    uses_logrocket: sniffed_data['logrocket'][:detected],
+    uses_newrelic: sniffed_data['newrelic'][:detected],
+    uses_datadog: sniffed_data['datadog'][:detected],
+    uses_bugsnag: sniffed_data['bugsnag'][:detected],
+    uses_sentry: sniffed_data["sentry"][:detected],
+    sentry_sdk_version: sniffed_data['sentry'][:sdk_version],
+    sentry_dsn_host: sniffed_data['sentry'][:dsn_host],
+    sentry_project_id: sniffed_data['sentry'][:project_id]
   }
   formatted_list << org_details
 end
